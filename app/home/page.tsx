49a3867033d446eaed327347418ab55706c5e09e
Buddhika-BistecGlobal <102160694+Buddhika-BistecGlobal@users.noreply.github.com>
2024-04-03 19:48:30 +0530

    PendingVersion_dev

---
+++ app/home/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { home } from '@bistecAudutMangment-app/pages';
+function page() {
+  return (
+    <home />
+  )
+}
+export default page
---
+++ src/Pages/Home/index.tsx
@@ -0,0 +1,12 @@
+
+import supabase from '@bistecAudutMangment-app/config';
+
+ function Home() {
+  console.log(supabase)
+  return(
+    <div className="page Home">
+      <h2>Home</h2>
+    </div>
+  )
+}
+export default Home
